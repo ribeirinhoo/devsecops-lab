a0e2512c2235d1ba577b9ea2dd0ee6f76e322686
Gabriel Ribeiro <gribeirogarcia@gmail.com>
2026-02-26 08:22:08 -0300

    lab inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const _ = require('lodash');
+const app = express();
+
+app.use(express.json());
+
+// VULNERABILIDADE: eval com input do usuário
+app.post('/calc', (req, res) => {
+    const result = eval(req.body.expression);
+    res.json({ result });
+});
+
+app.get('/', (req, res) => {
+    res.send("API vulnerável rodando");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
